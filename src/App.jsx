d287766e53a3c56b873c0d480a5481cd44dafc87
Sumit Chalotra <kumar.sumit9981@gmail.com>
2025-07-05 20:33:24 +0530

    added seo + semantic tags

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,21 @@ import underConstructionGif from './assets/under-construction-img.gif'
 function App() {
   return (
     <div className="under-construction-container">
-      <img src={logoImg} alt="logo" className="logo-image" />
-      <img src={underConstructionGif} alt="Under Construction Animation" className="construction-gif" />
-      <p>We're working hard to bring you something awesome!<br/>Please check back soon.</p>
+      <header className="site-header">
+        <img src={logoImg} alt="Vasu EA Tax Academy Logo" className="logo-image" />
+      </header>
+      
+      <main className="main-content">
+        <section className="construction-notice">
+          <img src={underConstructionGif} alt="Under Construction Animation" className="construction-gif" />
+          <h1 className="sr-only">Vasu EA Tax Academy - Coming Soon</h1>
+          <p>We're working hard to bring you something awesome!<br/>Please check back soon.</p>
+        </section>
+      </main>
+      
+      <footer className="site-footer">
+        <p>&copy; 2024 Vasu EA Tax Academy. All rights reserved.</p>
+      </footer>
     </div>
   )
 }
