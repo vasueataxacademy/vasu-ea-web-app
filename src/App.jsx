ef90b1af8ae3d4fb7f6bb05ccfd9112034d9567b
Sumit Chalotra <kumar.sumit9981@gmail.com>
2025-07-05 20:21:51 +0530

    added under construction banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 // import constructionImg from './assets/construction.svg'
 import logoImg from './assets/logo.jpeg'
+import underConstructionGif from './assets/under-construction-img.gif'
 
 function App() {
   return (
     <div className="under-construction-container">
-      <img src={logoImg} alt="Under Construction" className="construction-image" />
-      <h1>Under Construction</h1>
+      <img src={logoImg} alt="logo" className="logo-image" />
+      <img src={underConstructionGif} alt="Under Construction Animation" className="construction-gif" />
       <p>We're working hard to bring you something awesome!<br/>Please check back soon.</p>
     </div>
   )
